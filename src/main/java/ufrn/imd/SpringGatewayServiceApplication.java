88efde96b207ee388a2664ff46178181287bc477
Minobarbarian <iolanda.chagas@nibo.com.br>
2025-01-22 13:00:52 -0300

    some tolerance

--- src/main/java/ufrn/imd/SpringGatewayServiceApplication.java
+++ src/main/java/ufrn/imd/SpringGatewayServiceApplication.java
@@ -3,6 +3,9 @@ package ufrn.imd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,12 @@ public class SpringGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGatewayServiceApplication.class, args);
+	}
+	
+	@Bean
+    @LoadBalanced
+    RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
